448fcf54cc7b1a825967cd92364bb78a36b68e88
ahnadiri <108028111+Abbas-Nadiri@users.noreply.github.com>
2024-06-25 23:52:32 +0100

    create grid with variable no. of squares based on user input

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const body = document.body;
+const root = document.querySelector(":root");
+const rootStyles = getComputedStyle(root);
 
 let container = document.querySelector("#container");
 
-for (let i = 0; i < 16; i++) {
+let x = prompt("Enter grid size:");
+
+root.style.setProperty("--gridSize", x)
+let gridSize = rootStyles.getPropertyValue("--gridSize");
+
+for (let i = 0; i < gridSize; i++) {
     let newDiv = document.createElement("div");
     newDiv.className = "square";
     container.append(newDiv);
