419a03efeafafef24310aa56c6ce2c438ac76f3b
ahnadiri <108028111+Abbas-Nadiri@users.noreply.github.com>
2024-06-26 10:07:21 +0100

    set 100x100 limit on grid generation

--- script.js
+++ script.js
@@ -4,13 +4,18 @@ const rootStyles = getComputedStyle(root); /* access CSS :root style variables*/
 
 let container = document.querySelector("#container");
 
-let x = prompt("Enter grid size:") ** 2; 
+
+let x = NaN;
+while(x > 10000 || isNaN(x)) {
+    x = prompt("Enter grid size (100x100 maximum):") ** 2; 
+}
 
 root.style.setProperty("--gridSize", x); /* changes --gridSize CSS variable to user input */
 let gridSize = rootStyles.getPropertyValue("--gridSize"); /* obtains latest size for gridSize CSS variable */
 
 function createGrid(size) { 
     for (let i = 0; i < size; i++) {
+
         let newDiv = document.createElement("div");
         newDiv.className = "square";
         container.append(newDiv);
@@ -29,15 +34,28 @@ button.textContent = "Reset/Change grid size";
 body.append(button);
 
 button.addEventListener("click", () => {
-    const delet = document.getElementsByClassName("square");
-    while(delet.length > 0) {
-        delet[0].parentNode.removeChild(delet[0]);
-    };
+let newGridSize = prompt("Enter new grid size:") ** 2
 
-    let newGridSize = prompt("Enter new grid size:") ** 2
-    root.style.setProperty("--gridSize", newGridSize);
-    createGrid(newGridSize);
-})
+    if(newGridSize > 10000) {
+
+        alert("Grid size too large, maximum 100x100. Enter a different number.");
+
+    } else if (isNaN(newGridSize)){
+
+        alert("That's not a number...")
+        
+    } else {
+
+        const delet = document.getElementsByClassName("square");
+        while(delet.length > 0) {
+            delet[0].parentNode.removeChild(delet[0]);
+        };
+
+        root.style.setProperty("--gridSize", newGridSize);
+        createGrid(newGridSize);
+
+    };
+});
 
 
 
